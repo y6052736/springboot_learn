e03fb519bed50082e0a6f2287bfb768b13c0a6d3
Yangjie <y6052736@126.com>
2018-03-19 16:51:17 +0800

    init import

---
+++ demo/src/main/java/com/example/demo/controller/MyConttroller.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by 000489 on 2018/1/11.
+ */
+@Controller
+@RequestMapping("/demo")
+public class MyConttroller {
+
+    @RequestMapping("/test")
+    @ResponseBody
+    public String test() {
+        return "hello world";
+    }
+
+}
